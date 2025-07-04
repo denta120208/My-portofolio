80f4d9db728acb978062879edd546716e4c07510
denta120208 <ardentapradan@gmail.com>
2025-07-04 16:34:57 +0700

    New Projek add

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
     Typed Js 
 ====================== */
 const typed = new Typed(".multiple-text", {
-  strings: ["Frontend Developer", "Backend Developer", "IT Support"],
+  strings: ["Full Stack Developer", "Mobile Developer", "Backend Developer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
