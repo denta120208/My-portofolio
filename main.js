d98000ab1b5d1244c8b2699af9e90ffcdb4501a1
denta120208 <ardentapradan@gmail.com>
2025-07-07 10:06:28 +0700

    New Text in About and skill

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
     Typed Js 
 ====================== */
 const typed = new Typed(".multiple-text", {
-  strings: ["Full Stack Developer", "Mobile Developer", "Backend Developer"],
+  strings: ["Full Stack Developer", "Mobile Developer", "IT Support"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
